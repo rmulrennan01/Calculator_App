09e2b059e6485645bf3cbe64d55d78b9a4fe0561
rmulrennan01 <rpmulrennan@gmail.com>
2022-01-14 15:29:23 -0600

    updated formatting for history section

--- src/HistoryDisplay.js
+++ src/HistoryDisplay.js
@@ -9,10 +9,10 @@ function HistoryDisplay(props) {
     const printCalcs = (n) =>{
         if(n.prev != null){
         return(
-            <>
+            <div className="historyDisplay__row" onClick={()=>alert(n.answer)}>
             {n.prev} {n.operator} {n.current} = {n.answer}
             <br/> 
-            </> 
+            </div> 
         ) ; 
         }
 
